509befea10fdaea0f9649287a22dcc6d01b58949
Max van Haandel <max.vanhaandel@student.fontys.nl>
2023-04-04 13:00:36 +0200

    stuff for homepage

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("Homepage.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error_404.html"), 404
+
 if __name__ == ("__main__"):
     app.run()
--- static/script/jscript.js
+++ static/script/jscript.js
@@ -1,3 +1,4 @@
+//Manual slideshow
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -23,4 +24,25 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+}
+
+//Auto slideshow
+let autoIndex = 0;
+autoSlides();
+
+function autoSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  autoIndex++;
+  if (autoIndex > slides.length) {autoIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[autoIndex-1].style.display = "block";  
+  dots[autoIndex-1].className += " active";
+  setTimeout(autoSlides, 5000); // Change image every 5 seconds
 }
